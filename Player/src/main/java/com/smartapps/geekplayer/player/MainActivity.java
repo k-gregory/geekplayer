694c4f8f26f838713b2b981c779d3dae2df27a81
Gregory Komar <xalvevs@gmail.com>
2015-08-17 01:10:11 +0300

    Player Fragment .Linear Layout canceled.

--- Player/src/main/java/com/smartapps/geekplayer/player/MainActivity.java
+++ Player/src/main/java/com/smartapps/geekplayer/player/MainActivity.java
@@ -1,15 +1,28 @@
 package com.smartapps.geekplayer.player;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 
 /**
  * Created by gregory on 8/15/15.
  */
 public class MainActivity extends Activity {
+
+    private class DrawerClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id){
+            selectItem(position);
+        }
+    }
+
     private String[] menuEntries;
     private DrawerLayout drawerLayout;
     private ListView drawerList;
@@ -21,5 +34,16 @@ public class MainActivity extends Activity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerList = (ListView) findViewById(R.id.menu_drawer);
         drawerList.setAdapter(new ArrayAdapter<>(this,R.layout.drawer_list_item,menuEntries));
+        drawerList.setOnItemClickListener(new DrawerClickListener());
+        selectItem(0);
+    }
+
+    private void selectItem(int position){
+        Fragment fragment = new PlayFragment();
+        FragmentManager manager = getFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        drawerLayout.closeDrawer(drawerList);
     }
 }
--- Player/src/main/java/com/smartapps/geekplayer/player/PlayFragment.java
+++ Player/src/main/java/com/smartapps/geekplayer/player/PlayFragment.java
@@ -13,6 +13,6 @@ import android.view.ViewGroup;
 public class PlayFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_player,container);
+        return inflater.inflate(R.layout.fragment_player,container,false);
     }
 }
