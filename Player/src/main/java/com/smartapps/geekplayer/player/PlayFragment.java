cc39d94ba1e3a936e5ba4b659cf65bfe13b7a411
Gregory Komar <xalvevs@gmail.com>
2015-08-17 02:10:23 +0300

    Started work on audio playback

--- Player/src/main/java/com/smartapps/geekplayer/player/PlayFragment.java
+++ Player/src/main/java/com/smartapps/geekplayer/player/PlayFragment.java
@@ -1,18 +1,63 @@
 package com.smartapps.geekplayer.player;
 
 import android.app.Fragment;
+import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by gregory on 8/16/15.
  */
 
 public class PlayFragment extends Fragment {
+
+    private MediaPlayer player;
+
+    public void playTrack(String path) throws IOException{
+        player.reset();
+        player.setDataSource(path);
+        player.prepare();
+        player.start();
+    }
+
+    private String[] getAudioList() {
+        final Cursor mCursor = getActivity().getContentResolver().query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                new String[]{MediaStore.Audio.Media.DISPLAY_NAME, MediaStore.Audio.Media.DATA}, null, null,
+                "LOWER(" + MediaStore.Audio.Media.TITLE + ") ASC");
+
+        int count = mCursor.getCount();
+
+        String[] songs = new String[count];
+        String[] mAudioPath = new String[count];
+        int i = 0;
+        if (mCursor.moveToFirst()) {
+            do {
+                songs[i] = mCursor.getString(mCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME));
+                mAudioPath[i] = mCursor.getString(mCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+                i++;
+            } while (mCursor.moveToNext());
+        }
+
+        mCursor.close();
+
+        return songs;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        player = new MediaPlayer();
+        try {
+            playTrack(getAudioList()[0]);
+        } catch (IOException e) {
+
+        }
         return inflater.inflate(R.layout.fragment_player,container,false);
     }
 }
