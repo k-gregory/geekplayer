6e0671e7ac51c9c30ff6e37831d2d5e0df5513d0
Gregory Komar <xalvevs@gmail.com>
2015-08-16 21:05:01 +0300

    Started Player Fragment

---
+++ Player/src/main/java/com/smartapps/geekplayer/player/PlayFragment.java
@@ -0,0 +1,18 @@
+package com.smartapps.geekplayer.player;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gregory on 8/16/15.
+ */
+
+public class PlayFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_player,container);
+    }
+}
