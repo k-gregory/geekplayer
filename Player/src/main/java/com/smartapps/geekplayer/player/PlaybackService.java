074696bebf280b7e6bba1294eca8515f7d814897
Gregory Komar <xalvevs@gmail.com>
2015-08-26 01:40:22 +0300

    Playlist

--- Player/src/main/java/com/smartapps/geekplayer/player/PlaybackService.java
+++ Player/src/main/java/com/smartapps/geekplayer/player/PlaybackService.java
@@ -1,21 +1,44 @@
 package com.smartapps.geekplayer.player;
 
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
 
 /**
  * Created by gregory on 8/17/15.
  */
-public class PlaybackService extends IntentService {
+public class PlaybackService extends Service {
     private MediaPlayer player;
+    private IBinder m_binder;
 
-    public  PlaybackService(){
-        super("GP_PlaybackService");
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    private Playlist playlist;
+
+    public void play(){
+
+    }
+
+    public void pause(){
+
+    }
+
+    public class PlaybackServiceBinder extends Binder {
+        public PlaybackService getService(){
+            return PlaybackService.this;
+        }
+    }
+
+    public PlaybackService(){
         player = new MediaPlayer();
     }
     @Override
-    public void onHandleIntent(Intent intent) {
-
+    public IBinder onBind(Intent intent){
+        return m_binder;
     }
 }
