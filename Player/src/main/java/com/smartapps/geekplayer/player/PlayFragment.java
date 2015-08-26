99067a9d29cc6a558e49149ca260bfd428391b84
Gregory <xalvevs@gmail.com>
2015-08-26 16:11:35 +0300

    Playback service

--- Player/src/main/java/com/smartapps/geekplayer/player/PlayFragment.java
+++ Player/src/main/java/com/smartapps/geekplayer/player/PlayFragment.java
@@ -1,11 +1,15 @@
 package com.smartapps.geekplayer.player;
 
 import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,6 +26,23 @@ import java.io.IOException;
 public class PlayFragment extends Fragment {
 
     private PlaybackService playbackService;
+    private boolean pbServiceBound = false;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            PlaybackService.PlaybackServiceBinder binder =
+                    (PlaybackService.PlaybackServiceBinder) iBinder;
+            playbackService = binder.getService();
+            playbackService.play();
+            Log.e("service","connected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -31,8 +52,9 @@ public class PlayFragment extends Fragment {
     public void onStart(){
         super.onStart();
         Intent intent = new Intent(getActivity(),PlaybackService.class);
+        getActivity().bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
     @Override public void onStop() {
-
+        super.onStop();
     }
 }
